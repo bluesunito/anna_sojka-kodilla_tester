4dadbac9e035af7e9acb9f1a9a5170f4b35f4d7a
Anna Sojka <dovlamore@gmail.com>
2026-02-01 12:13:28 +0100

    "MainExtended"

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,17 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String bandName = "Talking Heads";
+        String placeFormed = "New York";
+        int dateCreated = 1975;
+        int dateDisbanded = 1991;
+        String nameVocalist = "David Byrne";
+        String nameFirstAlbum = "Talking Heads: 77";
+
+        System.out.println(bandName);
+        System.out.println(placeFormed);
+        System.out.println(dateCreated);
+        System.out.println(dateDisbanded);
+        System.out.println(nameVocalist);
+        System.out.println(nameFirstAlbum);
+    }
+}
