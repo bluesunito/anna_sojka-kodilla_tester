608fe8d3cef73d32c50d7886c92ffc9816d6a698
Anna Sojka <dovlamore@gmail.com>
2026-02-17 15:28:08 +0100

    Modul 2.6

---
+++ kodilla-intro/src/main/Module2/src/main/java/Module26/Users.java
@@ -0,0 +1,22 @@
+package Module26;
+
+public class Users{
+    String name;
+    double age;
+    double height;
+
+    public Users (String name, double age, double height){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public void checkUser (){
+        if (this.age > 30 && this.height > 160){
+            System.out.println("User is older than 30 and taller than 160cm");
+        } else {
+            System.out.println("User is 30 (or younger) or 160cm (or shorter)");
+        }
+    }
+
+}
---
+++ kodilla-intro/src/main/Module2/src/main/java/Module26/UsersApplication.java
@@ -0,0 +1,8 @@
+package Module26;
+
+public class UsersApplication {
+    public static void main(String[] args) {
+        Users userOne = new Users("Adam", 40.5,178);
+        userOne.checkUser();
+    }
+}
