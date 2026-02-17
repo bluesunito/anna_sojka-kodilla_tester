6d6acc10fa8b263f55f0bbb02d3cc0a4c0f499f8
Anna Sojka <dovlamore@gmail.com>
2026-02-17 15:56:21 +0100

    Modul 2.4

---
+++ kodilla-intro/src/main/Module2/src/main/java/Module24/Grades.java
@@ -0,0 +1,41 @@
+package Module24;
+
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+
+    public int lastGrade(){
+        if (this.size > 0){
+            int lastGrade = this.grades[this.size];
+            return lastGrade;
+        }else {
+            System.out.println("Student has no grades yet.");
+        }
+    }
+
+    public int calculateAvg(){
+        if (this.size > 0){
+            int sum = 0;
+            for (int n = 0; n<this.size; n++){
+            sum += this.grades[n];
+            double average = sum / this.size;
+            return average;
+            }
+        }else {
+            System.out.println("Student has no grades yet, no average available.");
+        }
+
+    }
+}
