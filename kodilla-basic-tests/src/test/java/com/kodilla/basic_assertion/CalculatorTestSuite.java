ea3f3ab1ef3d4ce88ae39de0e05fc8c0206542c2
Anna Sojka <dovlamore@gmail.com>
2026-02-24 17:05:44 +0100

    Modul 3.6 z komentarzami

--- kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
+++ kodilla-basic-tests/src/test/java/com/kodilla/basic_assertion/CalculatorTestSuite.java
@@ -8,51 +8,58 @@ public class CalculatorTestSuite {
 
     @Test
     public void testSum() {
+        // given
         Calculator calculator = new Calculator();
         int a = 5;
         int b = 8;
+        // when
         int sumResult = calculator.sum(a, b);
+        // then
         assertEquals(13, sumResult);
     }
 
     @Test
     public void testSubtract() {
+        // given
         Calculator calculator = new Calculator();
         int a = 5;
         int b = 8;
-
+        // when
         int subResult = calculator.subtract(a, b);
-
+        // then
         assertEquals(-3, subResult);
     }
 
     @Test
     public void testSquaredForPositiveNumber() {
+        // given
         Calculator calculator = new Calculator();
         int a = 5;
-
+        // when
         double squareResult = calculator.squared(a);
-
+        // then
         assertEquals(25.0, squareResult, 0.001);
     }
 
     @Test
     public void testSquaredForZero() {
+        // given
         Calculator calculator = new Calculator();
         int a = 0;
-
+        // when
         double squareResult = calculator.squared(a);
-
+        // then
         assertEquals(0.0, squareResult, 0.001);
     }
 
     @Test
     public void testSquaredForNegativeNumber() {
+        // given
         Calculator calculator = new Calculator();
         int a = -4;
-
+        // when
         double squareResult = calculator.squared(a);
-
+        // then
         assertEquals(16.0, squareResult, 0.001);
     }
 }
